4083c721326b69cfcd42cb618b290d49b7a83b57
sjtg <chipilej@gmail.com>
2018-10-15 17:14:10 +0200

    Added save feature to the publisher.entity

--- pro/src/src/publishers/publisher.entity.ts
+++ pro/src/src/publishers/publisher.entity.ts
@@ -1,5 +1,10 @@
-import {BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
-import {UserEntity} from '../../users/users.entity';
+import { BaseEntity, Column, Entity, ManyToOne, PrimaryGeneratedColumn} from 'typeorm';
+import { UserEntity} from '../../users/users.entity';
+import { CreatePublisherDto } from 'dto/create.dto';
+import { AppErrorTypeEnum } from 'commons/error/AppErrorTypeEnum';
+import { AppError } from 'commons/error/AppError';
+
+
 @Entity({name: 'publishers'})
     export class PublishersEntity extends BaseEntity{
         @PrimaryGeneratedColumn()
@@ -16,4 +21,26 @@ import {UserEntity} from '../../users/users.entity';
         isPublished: boolean;
         @ManyToOne(type => UserEntity)
         user: UserEntity;
+
+
+       public static async createPublishers(publishers: CreatePublisherDto[], user: UserEntity): Promise<PublishersEntity[]> {
+       const u: UserEntity = await UserEntity.findOne(user.id);
+       if (!u) throw new AppError(AppErrorTypeEnum.USER_NOT_FOUND);
+       const publishersEntities: PublishersEntity[] = [];
+       publishers.forEach((p: CreatePublisherDto) => {
+           const pr: PublishersEntity = new PublishersEntity();
+           pr.id = p.id;
+           pr.bookname = p.bookname;
+           pr.genre = p.genre;
+           pr.description = p.description;
+           pr.views = p.views;
+           pr.isPublished = p.isPublished;
+           publishersEntities.push(pr);
+       });
+       u.publishers = publishersEntities;
+       const result: PublishersEntity[] = await PublishersEntity.save(publishersEntities);
+       await UserEntity.save([u]);
+       return Promise.all(result);
+   			
+   	}
     }
