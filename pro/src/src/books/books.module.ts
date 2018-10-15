2f480e40ddca76921af2ed8b99e58d0147a1b4e0
sjtg <chipilej@gmail.com>
2018-10-15 14:12:41 +0200

    updated books module

--- pro/src/src/books/books.module.ts
+++ pro/src/src/books/books.module.ts
@@ -1,31 +1,29 @@
 import { Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
 import { BooksController } from 'src/books/books.controller';
 import { BooksService } from 'src/books.service'; 
-
+ import {TypeOrmModule} from '@nestjs/typeorm';
+ import { BooksEntity } from 'src/books/books.entity';
 import { booksProviders } from 'src/providers';
 import { DatabaseModule } from 'database/database.module';
-// import { BookSchema } from  '../../schema/all.schema';
-
-
-
-
 
 @Module({
 	
-	imports: [DatabaseModule],
-	controllers: [BooksController],
-	providers: [BooksService, ...booksProviders],
 	
-
+	  imports: [TypeOrmModule.forFeature([BooksEntity])],
+      controllers: [ProjectController],
+      providers: [ProjectService]
 
 })
 export class BooksModule {}
 
 
 
+    
+
 
 // import { TypeOrmModule } from '@nestjs/typeorm';
+// import { BookSchema } from  '../../schema/all.schema';
+// import { MongooseModule } from '@nestjs/mongoose';
 // import { Book } from 'src/books/books.entity';
 
 // imports : [MongooseModule.forFeature([{ name : 'Book', schema: BookSchema}])],
@@ -36,5 +34,24 @@ export class BooksModule {}
 // 	providers: [BooksService],
 // 	exports : [BooksService]
 
+// import { Module } from '@nestjs/common';
+//     import { ProjectController } from './project.controller';
+//     import { ProjectService } from './project.service';
+//     import {TypeOrmModule} from '@nestjs/typeorm';
+//     import {ProjectEntity} from './project.entity';
+//     @Module({
+//         imports: [TypeOrmModule.forFeature([ProjectEntity])],
+//         controllers: [ProjectController],
+//         providers: [ProjectService]
+//     })
+//     export class ProjectModule {}
+
+
+
+
+// imports: [DatabaseModule],
+// 	controllers: [BooksController],
+// 	providers: [BooksService, ...booksProviders],
+	
 
 // })
