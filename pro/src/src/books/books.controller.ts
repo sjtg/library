4ea370f654b15cfc177699070d67bb3d4c59a116
sjtg <chipilej@gmail.com>
2018-10-15 17:38:27 +0200

    Modified books and publisher controllers

--- pro/src/src/books/books.controller.ts
+++ pro/src/src/books/books.controller.ts
@@ -1,16 +1,28 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body , HttpStatus, Res, UseGuards} from '@nestjs/common';
+import {SessionGuard} from '../../auth/SessionGuard';
+import {SessionUser} from '../../users/user.decorator';
+import { UserEntity} from '../../users/users.entity';
 import { CreateBookDto } from '../../dto/create.dto';
 import { BooksService } from '../books.service';
 import { Book as BookInterface } from '../../interfaces/interface';
+import { BooksEntity } from '../books/books.entity';
 // import { ValidationPipe } from '../../commons/pipes';
 @Controller('books')
 export class BooksController {
 
 constructor(private readonly booksService: BooksService) {}
+  
+    @Post('')
+    @UseGuards(SessionGuard)
+    public async createBooks(@Body() createBooks: CreateBookDto[], @Res() res, @SessionUser() user: UserEntity) {
+        const books: BooksEntity[] = await this.booksService.createBooks(createBooks, user);
+        return res.status(HttpStatus.OK).send(books);
+    }
  
 	// Post New books 
-  @Post()
+  @Post('')
   // @UsePipes(new ValidationPipe())
+
   async create(@Body() createBookDto: CreateBookDto) {
   	this.booksService.create(createBookDto);
   }
--- pro/src/src/publishers/publishers.controller.ts
+++ pro/src/src/publishers/publishers.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Post, Body} from '@nestjs/common';
+import { Controller, Get, Post, Body , HttpStatus, Res, UseGuards} from '@nestjs/common';
+import {SessionGuard} from '../../auth/SessionGuard';
+import {SessionUser} from '../../users/user.decorator';
+import { UserEntity} from '../../users/users.entity';
 import { CreatePublisherDto } from '../../dto/create.dto';
 import { PublishersService } from '../publishers.service';
 import { Publisher  as PublisherInterface } from '../../interfaces/interface';
+import { PublishersEntity } from '../publishers/publisher.entity';
 @Controller('publishers')
 
 export class PublishersController {
 
 	constructor(private readonly publisersService: PublishersService) {}
+
+  @Post('')
+    @UseGuards(SessionGuard)
+    public async createPublishers(@Body() createPublishers: CreatePublisherDto[], @Res() res, @SessionUser() user: UserEntity) {
+        const publishers: PublishersEntity[] = await this.publisersService.createPublishers(createPublishers, user);
+        return res.status(HttpStatus.OK).send(publishers);
+    }
 
 	  // Post New books 
   @Post()
