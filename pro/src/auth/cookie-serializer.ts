44977d146b1dcaf1cc2f8897957b1b0c6ec6b1de
sjtg <chipilej@gmail.com>
2018-10-16 12:19:53 +0200

    Added passport package

--- pro/src/auth/cookie-serializer.ts
+++ pro/src/auth/cookie-serializer.ts
@@ -1,11 +1,12 @@
     // import {PassportSerializer} from '@nestjs/passport/dist/passport.serializer';
-    // import {Injectable} from '@nestjs/common';
-    // @Injectable()
-    // export class CookieSerializer extends PassportSerializer {
-    //     serializeUser(user: any, done: Function): any {
-    //         done(null, user);
-    //     }
-    //     deserializeUser(payload: any, done: Function): any {
-    //         done(null, payload);
-    //     }
-    // }
+     import { PassportSerializer } from '@nestjs/passport';
+    import {Injectable} from '@nestjs/common';
+    @Injectable()
+    export class CookieSerializer extends PassportSerializer {
+        serializeUser(user: any, done: Function): any {
+            done(null, user);
+        }
+        deserializeUser(payload: any, done: Function): any {
+            done(null, payload);
+        }
+    }
